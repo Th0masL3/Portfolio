a3126d66bde488383afd81d34e8df6fdfb711a2f
Ricardo Falcao <ricardolfalcao.2005@gmail.com>
2024-12-04 19:57:01 -0500

    Attempt to fix everything with andrew

---
+++ src/main/java/com/example/gameoverstore/backend/Products/dataacesslayer/Console.java
@@ -0,0 +1,26 @@
+package com.example.gameoverstore.backend.Products.dataacesslayer;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Document(collection = "consoles")
+public class Console {
+
+    @Id
+    private String id;
+    //check for id
+    private String consoleId;
+    private String consoleName;
+    private LocalDate releaseDate;
+    private double price;
+    private int quantityInStock;
+    private String company;
+}
