1809a1598acc8125318985746eb6f5761794cefe
AndrewBadIdea <andrewbadea1996@gmail.com>
2024-12-10 22:29:42 -0500

    Nearly finished the front end part

---
+++ fe-lecoingameover/src/pages/Auth/AuthButton.tsx
@@ -0,0 +1,16 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+const AuthButton = (): JSX.Element => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return isAuthenticated ? (
+    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      Log Out
+    </button>
+  ) : (
+    <button onClick={() => loginWithRedirect()}>Log In</button>
+  );
+};
+
+export default AuthButton;
---
+++ fe-lecoingameover/src/pages/Navigation.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import AuthButton from "./Auth/AuthButton";
+import Profile from "./Auth/Profile";
+import "./Navigation.css"; 
+
+const Navigation = (): JSX.Element => {
+  return (
+    <nav className="navigation-bar">
+      <div className="logo-container">
+        {/* Add logo image */}
+        <img src="./logo.jpg" alt="Logo" className="logo" />
+      </div>
+      <div className="nav-links">
+        <Link to="/consoles">Consoles</Link>
+      </div>
+      <div className="nav-actions">
+        <AuthButton />
+        <div className="profile-pic">
+  <Profile />
+</div>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
